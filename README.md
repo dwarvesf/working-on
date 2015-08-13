69a55567c3f7623a6f86557f34c10fe94a3b2205
Han Ngo <nntruonghan@gmail.com>
2015-08-13 19:50:36 +0700

    add digest()

--- README.md
+++ README.md
@@ -14,18 +14,21 @@ Working On app that help the team improve the productivity
 
 
 1. Add Bot
+
 - Add new integration: Bots. The bot will post the digest and remind messages everyday.
 - Retrieve API Token
 - Back to Slack and invite the bot to the channel
 
 2. Add Slash Command
+
 - Add new integration: Slash Commands.
 - Retrieve the Token
 - Add url `<your-host>/d`
 
 3. Configure cli
+
 - Clone project [working-on-cli]()
-- Access https://api.slack.com/web your token.
+- Access https://api.slack.com/web to get your token.
 - Run ./setup.sh `<token>` will create bin file and config file for you.
 
 ### Digest for project channel
--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"os"
+	"strings"
 	"time"
 
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/bmuller/arrow/lib"
 	"github.com/carlescere/scheduler"
 	"github.com/gin-gonic/contrib/ginrus"
 	"github.com/gin-gonic/gin"
+	"github.com/nlopes/slack"
 )
 
 const (
@@ -29,9 +32,7 @@ func main() {
 	}
 
 	// Setup schedule jobs
-	remindJob := remind
 	digestJob := digest
-	scheduler.Every().Day().At("23:00").Run(remindJob)
 	scheduler.Every().Day().At("09:30").Run(digestJob)
 
 	// Prepare router
@@ -55,6 +56,14 @@ func getDBConnection() *mgo.Database {
 	return session.DB(dbName)
 }
 
+type Item struct {
+	ID        bson.ObjectId `json:"id" bson:"_id"`
+	UserID    string        `json:"user_id" bson:"user_id"`
+	Name      string        `json:"user_name" bson:"user_name"`
+	Text      string        `json:"text" bson:"text"`
+	CreatedAt time.Time     `json:"created_at" bson:"created_at"`
+}
+
 // Message will be passed to server with '-' prefix via various way
 //	+ Direct message with the bots
 //	+ Use slash command `/working <message>`
@@ -64,30 +73,30 @@ func getDBConnection() *mgo.Database {
 // Token is secondary param to indicate the user
 func addDoneItem(ctx *gin.Context) {
 
-	type Item struct {
-		ID        bson.ObjectId `json:"id" bson:"_id"`
-		UserID    string        `json:"user_id" bson:"user_id"`
-		Name      string        `json:"user_name" bson:"user_name"`
-		Text      string        `json:"text" bson:"text"`
-		CreatedAt time.Time     `json:"created_at" bson:"created_at"`
-	}
-
 	// Parse token and message
 	var item Item
-	if ctx.Bind(&item) == nil {
+	if ctx.Bind(&item) != nil {
+		log.Fatal("Cannot parse data")
+	}
 
-		// Add Item to database
-		session, err := mgo.Dial(dbURL)
-		if err != nil {
-			panic(err)
-		}
-		defer session.Close()
+	item.CreatedAt = time.Now()
 
-		c := session.DB(dbName).C("items")
-		err = c.Insert(item)
-		if err != nil {
-			log.Fatalln(err)
-		}
+	message := strings.TrimSpace(item.Text)
+	if !strings.HasPrefix(message, "-") {
+		panic("Wrong format: " + item.Text)
+	}
+
+	// Add Item to database
+	session, err := mgo.Dial(dbURL)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	c := session.DB(dbName).C("items")
+	err = c.Insert(item)
+	if err != nil {
+		log.Fatalln(err)
 	}
 }
 
@@ -103,12 +112,74 @@ func login(c *gin.Context) {
 	// Return access token for later usage
 }
 
-// Post reminder to Slack channel
-func remind() {
-
-}
-
 // Post summary to Slack channel
 func digest() {
 
+	channel := "#general"
+	botToken := os.Getenv("BOT_TOKEN")
+
+	if botToken == "" {
+		log.Fatal("No token provided")
+		os.Exit(1)
+	}
+
+	s := slack.New(botToken)
+	users, err := s.GetUsers()
+
+	if err != nil {
+		log.Fatal("Cannot get users")
+		os.Exit(1)
+	}
+
+	itemCollection := getDBConnection().C("items")
+
+	// If count > 0, it means there is data to show
+	count := 0
+	title := "Yesterday I learnt"
+	params := slack.PostMessageParameters{}
+	fields := []slack.AttachmentField{}
+
+	// Prepare attachment of done items
+	for _, user := range users {
+
+		if user.IsBot || user.Deleted {
+			continue
+		}
+
+		// Query done items from Database
+		var values string
+		var items []Item
+		err = itemCollection.Find(bson.M{"user_id": user.Id, "created_at": bson.M{"$gt": arrow.Yesterday()}}).All(&items)
+
+		if err != nil {
+			log.Fatal("Cannot query done items.")
+			os.Exit(1)
+		}
+
+		if len(items) > 0 {
+			count = count + 1
+		}
+
+		for _, item := range items {
+			values = values + item.Text + "\n"
+		}
+
+		field := slack.AttachmentField{
+			Title: user.Name,
+			Value: values,
+		}
+
+		fields = append(fields, field)
+	}
+
+	params.Attachments = []slack.Attachment{
+		slack.Attachment{
+			Color:  "#7CD197",
+			Fields: fields,
+		},
+	}
+
+	if count > 0 {
+		s.PostMessage(channel, title, params)
+	}
 }
